99fe28883da797a4b2c2c998a65b0c3f42cafe70
Tamer <tamerardaal@gmail.com>
2021-05-22 16:23:02 +0300

    buton parametreleri oluşturuldu

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -4,21 +4,24 @@ class MyButton extends StatelessWidget {
   final color, textColor;
   final String buttonText;
 
-  MyButton(this.textColor, this.color, this.buttonText);
+  MyButton({this.textColor, this.color, this.buttonText});
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child: Container(
-        child: Center(
-          child: Text(
-            buttonText,
-            style: TextStyle(
-              color: textColor,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Container(
+          color: color,
+          child: Center(
+            child: Text(
+              buttonText,
+              style: TextStyle(
+                color: textColor,
+              ),
             ),
           ),
-
         ),
       ),
     );
